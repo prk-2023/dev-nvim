3356e97a718d91274682289f1cda2ddba46ccf84
ram <pulumati@realtek.com>
2024-04-30 08:58:40 +0800

    Daybreak neoVim

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Start RC file for nvim
+-- options: holds vim options and setting
+-- mapping: keymaps and shortcuts 
+	-- regular keymaps
+	-- maps for plugins
+-- plugings: 
+	-- install, update, setup, configure, keymaps( as comments )
+-- portability 
+	-- host the config at github
+
