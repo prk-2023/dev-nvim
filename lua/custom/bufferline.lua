ac837bfc784f1bda1a6799ebcb714f4482070333
devram <devslashram@gmail.com>
2024-08-19 21:47:55 +0800

    Fix error: remove colorscheme from bufferline

--- lua/custom/bufferline.lua
+++ lua/custom/bufferline.lua
@@ -3,20 +3,7 @@ return {
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
-		local c = require("rose-pine.palette")
 		require("bufferline").setup({
-			-- highlights = require("catppuccin.groups.integrations.bufferline").get({
-			-- 	styles = {},
-			-- 	custom = {
-			-- 		pine = {
-			-- 			buffer_selected = { fg = c.text, bg = c.base, bold = false, italic = false },
-			-- 			tab_close = { fg = c.surface1, bg = c.mantle, bold = false, italic = false },
-			-- 			close_button = { fg = c.surface1, bg = c.mantle, bold = false, italic = false },
-			-- 			close_button_visible = { fg = c.surface1, bg = c.mantle, bold = false, italic = false },
-			-- 			close_button_selected = { fg = c.surface1, bg = c.base, bold = false, italic = false },
-			-- 		},
-			-- 	},
-			-- }),
 			options = {
 				buffer_close_icon = "󰅖",
 				modified_icon = "● ",
@@ -31,15 +18,4 @@ return {
 			},
 		})
 	end,
-	-- options = {
-	-- 	indicator = {
-	-- 		icon = "▎", -- this should be omitted if indicator style is not 'icon'
-	-- 		style = "icon" | "underline" | "none",
-	-- 	},
-	-- 	buffer_close_icon = "󰅖",
-	-- 	modified_icon = "● ",
-	-- 	close_icon = " ",
-	-- 	left_trunc_marker = " ",
-	-- 	right_trunc_marker = " ",
-	-- },
 }
