05970c4736842fcf40ad7e0d464d14dcb3f90fdc
pulumati <pulumati@realtek.com>
2025-07-28 11:07:43 +0800

    Apply recommended setting for working with markdown,typst files
    - recommended settings for soft-wrapping and clean word-wrapping,
      commonly used in tools like vim-markdown or MarkdownPreview.

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -1,20 +1,3 @@
--- set perticular mapping for markdown files.
---vim.opt.number = true
-if vim.api.nvim_win_get_width(0) > 117 then
-	vim.cmd([[TagbarToggle]])
-	vim.cmd([[TagbarOpen]])
-	-- print("Console width is larger than 117 columns!")
-	-- print("---------hello----------")
-end
-
--- keybind:
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>mt",
-	":lua require('misc-funs').generate_markdown_table()<CR>",
-	{ noremap = true, silent = true }
-)
-
 -- Enable soft line wrapping
 vim.opt_local.wrap = true
 
