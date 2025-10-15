a7fb5aae838b13abafc4028bfddc7525207e0e3c
devslashram <devslashram@gmail.com>
2025-10-15 21:19:41 +0800

    change position of floating window to top-

--- lua/custom/noice.lua
+++ lua/custom/noice.lua
@@ -4,6 +4,18 @@ return -- lazy.nvim
 	event = "VeryLazy",
 	opts = {
 		-- add any options here
+        views = { 
+            cmdline_popup = {
+                position = {
+                    row = 1,
+                    col = "50%", -- Can be number or string like "50%"
+      },
+      size = {
+        width = 60,
+        height = "auto",
+      },
+    },
+  },
 	},
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
